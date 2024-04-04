ebe381a2a18a09af162fc749df3f1c62363dfb27
Ted Feng <artisdom@gmail.com>
2024-04-04 21:53:40 +1300

    cargo add num

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,3 +1,17 @@
+fn square_loop(mut x: f64) {
+    loop {
+        x = x * x;
+    }
+}
+
+fn square_add_loop(c: f64) {
+    let mut x = 0.;
+
+    loop {
+        x = x * x + c;
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
